c0399822940dd2fde6dfe76d6349aa49be57d8f8
xiao.geng <xiao.geng@wormpex.com>
2019-07-28 21:32:58 +0800

    add

---
+++ leetcode/code/JavaScript/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  if (!nums) return 0;
+  // 指向非重复元素的游标
+  var i = 0;
+  // 指向数组的游标
+  var j = 0;
+
+  for (j = 1; j < nums.length; j++) {
+    if (nums[i] !== nums[j]) {
+      // 非重复元素，确定元素位置
+      if ((i + 1) !== j) {
+        // 交互元素
+        var temp = nums[i + 1];
+        nums[i + 1] = nums[j];
+        nums[j] = temp;
+      }
+      i++;
+    }
+  }
+  return (i + 1);
+}
