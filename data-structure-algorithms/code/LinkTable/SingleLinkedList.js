74955dd77c453670c0842821fdf685b067708c93
xiao.geng <xiao.geng@wormpex.com>
2019-08-04 13:38:56 +0800

    update

---
+++ data-structure-algorithms/code/LinkTable/SingleLinkedList.js
@@ -0,0 +1,148 @@
+/**
+ * @description 结点对象
+ * @param element {int} 存储的数据
+ */
+class Node {
+  constructor(element) {
+    this.element = element;
+    this.next = null;
+  }
+}
+
+/**
+ * @description 单链表的插入、删除、查找操作，链表中存储的是 int 类型的数据
+ */
+class SingleLinkedList {
+  constructor() {
+    // 头结点
+    this.head = new Node('head');
+  }
+
+  /**
+   * @description 查询数据 x 的位置
+   * @param {*} element 结点的值
+   * @returns {Node} node
+   */
+  findByValue(element) {
+    let currentNode = this.head.next;
+    while (currentNode && currentNode.element !== element) {
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+  }
+
+  /**
+   * @description 查找第 index 位置的结点，索引从 0 开始
+   * @param {int} index 位置
+   * @returns {Node} node
+   */
+  findByIndex(index) {
+    let currentNode = this.head.next;
+    let pos = 0;
+    while (currentNode && pos !== index) {
+      pos++;
+      currentNode = currentNode.next;
+    }
+    return currentNode;
+
+  }
+
+  /**
+   * @description 向链表尾追加一个结点
+   * @param {int} element 数据
+   */
+  append(element) {
+    const newNode = new Node(element);
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = newNode;
+  }
+
+  /**
+   * @description 指定元素向后插入
+   * @param {*} newElement 插入的元素
+   * @param {*} element  插入元素的下个结点元素
+   */
+  insert(newElement, element) {
+    const currentNode = this.findByValue(element);
+    if (!currentNode) return;
+    const newNode = new Node(newElement);
+    newNode.next = currentNode.next;
+    currentNode.next = newNode;
+  }
+
+  /**
+   * @description 查询指定元素的前驱结点
+   * @param {int} element 元素的值
+   * @returns {Node} node
+   */
+  findPrevious(element) {
+    let currentNode = this.head;
+    while(currentNode.next && currentNode.next.element !== element) {
+      currentNode =  currentNode.next;
+    }
+    return currentNode.next ? currentNode : null;
+  }
+
+  /**
+   * @description 删除指定值的结点
+   * @param {int} element
+   */
+  remove(element) {
+    const prevNode = this.findPrevious(element);
+    if (!prevNode) return;
+    prevNode.next = prevNode.next.next;
+  }
+
+  /**
+   * @description 判断链表是否为空
+   * @returns Boolean
+   */
+  isEmpty() {
+    return this.head.next === null;
+  }
+
+  /**
+   * @description 打印单链表
+   */
+  printList() {
+    // 忽略头指针的值
+    let currentNode = this.head.next;
+    while (currentNode) {
+      console.log(currentNode.element);
+      currentNode = currentNode.next;
+    }
+  }
+}
+
+/**
+ * 测试用例
+ */
+(function () {
+  const singleList = new SingleLinkedList();
+  console.log('-------------test: append------------');
+  singleList.append(10);
+  singleList.append(20);
+  singleList.append(30);
+  singleList.printList(); // 10, 20, 30
+  console.log('-------------test: insert------------');
+  singleList.insert(11, 10);
+  singleList.insert(31, 30);
+  singleList.insert(41, 40);
+  singleList.printList(); // 10, 11, 20, 30, 31
+  console.log('-------------test: remove------------');
+  singleList.remove(11);
+  singleList.remove(30);
+  singleList.printList(); // 10, 20, 31
+  console.log('-------------test: findByValue------------');
+  const p = singleList.findByValue(10);
+  console.log(p.element); // 10
+  console.log('-------------test: findByValue------------');
+  const p1 = singleList.findByIndex(2);
+  console.log(p1.element); // 31
+  console.log('-------------test: findPrevious------------');
+  const p2 = singleList.findPrevious(31);
+  console.log(p2.element); // 20
+}());
