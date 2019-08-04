6387ccf020339ffde6fa5d1d5ce0a5b9a2fba5b3
xiao.geng <xiao.geng@wormpex.com>
2019-08-05 06:52:42 +0800

    update

--- leetcode/code/JavaScript/reverse-linked-list.js
+++ leetcode/code/JavaScript/reverse-linked-list.js
@@ -14,11 +14,14 @@ var reverseList = function(head) {
   var newHead = null;
   // 遍历链表
   while(currentNode) {
+    // 暂存当前结点
     var tempNode = currentNode;
+    // 指向下一个结点
     currentNode = currentNode.next;
-
+    // 修改当前结点的 next 指针
     tempNode.next = newHead;
     newHead = tempNode;
   }
+  // 返回新的 head
   return newHead;
 };
