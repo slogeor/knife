ef509eac6160c34c9ab14bed47e880813d3daaa1
xiao.geng <xiao.geng@wormpex.com>
2019-08-13 08:27:36 +0800

    add

--- leetcode/README.md
+++ leetcode/README.md
@@ -2,6 +2,10 @@
 - [0026.remove-duplicates-from-sorted-array(删除排序数组中的重复项)](./doc/remove-duplicates-from-sorted-array.md)
 - [0088.merge-sorted-array(合并两个有序数组)](./doc/merge-sorted-array.md)
 
+#### 数据结构: 数组
+- [简单: (0001)两数之和](./doc/two-sum.md)
+
+
 #### 数据结构: 链表
 - [简单: (0206)反转链表](./doc/reverse-linked-list.md)
 - [简单: (0141)环形链表](./doc/linked-list-cycle.md)
---
+++ leetcode/code/JavaScript/two-sum.js
@@ -0,0 +1,77 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+/**
+ * 场景1: 适用于数组已有序
+ * 设置两个指针，头指针和尾指针
+ */
+var twoSum = function (nums, target) {
+  // 空判断
+  if (!nums || nums.length === 0) return [];
+
+  var start = 0;
+  var end = nums.length;
+
+  // 异常判断
+  if (nums[end] * 2 < target || nums[start] * 2 > target) return [];
+
+  while (end - start > 1) {
+    if (nums[start] + nums[end] > target) {
+      end--;
+    } else if (nums[start] + nums[end] < target) {
+      start++
+    }
+  }
+
+  if (nums[start] + nums[end] === target) {
+    return [start, end];
+  }
+  return [];
+};
+
+/**
+ * 适用任何场景，利用哈希，遍历两次
+ */
+var twoSum = function (nums, target) {
+  // 空判断
+  if (!nums || nums.length === 0) return [];
+  var helper = new Map();
+  // 第一次遍历，存储value 和 i
+  for (var i = 0; i < nums.length; i++) {
+    helper.set(nums[i], i);
+  }
+
+  // 第二次遍历，查找目标
+  for (var i = 0; i < nums.length; i++) {
+    var firstVal = nums[i];
+    var secondVal = target - firstVal;
+    // i !== helper.get(secondVal) 这个重要
+    if (helper.has(secondVal) && i !== helper.get(secondVal)) {
+      return [i, helper.get(secondVal)];
+    }
+  }
+  return [];
+};
+
+/**
+ * 借助哈希，一次遍历，需要使用哈希的 has 方法，判断key是否存
+ */
+var twoSum = function (nums, target) {
+  // 空判断
+  if (!nums || nums.length === 0) return [];
+  var helper = new Map();
+
+  for (var i = 0; i < nums.length; i++) {
+    var firstVal = nums[i];
+    var secondVal = target - firstVal;
+    if (helper.has(secondVal)) {
+      return [helper.get(secondVal), i];
+    } else {
+      helper.set(firstVal, i);
+    }
+  }
+  return [];
+};
